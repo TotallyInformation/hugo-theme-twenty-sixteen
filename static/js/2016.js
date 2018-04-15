c3a5138e3ad924d791b96d1871f68447e587dbc4
Julian Knight <jk.github@totallyinformation.com>
2018-04-15 18:06:43 +0100

    Add hover anchors to headings 1-4

--- static/js/2016.js
+++ static/js/2016.js
@@ -1,25 +1,32 @@
+function addAnchor(element) {
+    // NB: Have to add the Variation Selector after the link icon if we want it coloured via the CSS color attib
+    element.insertAdjacentHTML('beforeend', `<a href="#${element.id}" class="hanchor" ariaLabel="Anchor"> 🔗&#xFE0E;</a>` )
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Add anchor links to all headings
+    var headers = document.querySelectorAll('article h1[id], article h2[id], article h3[id], article h4[id]')
+    if (headers) headers.forEach(addAnchor)
 
     // Get all "navbar-burger" elements
     var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
     // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-  
-          // Get the target from the "data-target" attribute
-          var target = $el.dataset.target;
-          var $target = document.getElementById(target);
-  
-          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-          $el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
+    if ($navbarBurgers.length > 0) {  
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
         });
-      });
     }
   
   });
