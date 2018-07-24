085805ef131026c0a0b11bcaa7e1a7410dc6ce6a
Julian Knight <jk.github@totallyinformation.com>
2018-07-24 17:19:23 +0100

    Tidy

--- assets/js/2016.js
+++ assets/js/2016.js
@@ -4,10 +4,10 @@
  */
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Get all "navbar-burger" elements
+    // BulmaCSS responsive menu - Get all "navbar-burger" elements
     var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
     // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {  
+    if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
         $navbarBurgers.forEach(function ($el) {
             $el.addEventListener('click', function () {
@@ -23,5 +23,5 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
-  
+
   });
